24a09ecf3d21585a52a4764045808caec1f1263e
dimuro14 <luisdimuro@hotmail.com>
2025-06-26 19:07:22 -0300

    Segunda versión con semáforos

--- src/Horario.java
+++ src/Horario.java
@@ -55,7 +55,7 @@ public class Horario extends Thread {
             
             try {
                 
-                Thread.sleep(100);
+                Thread.sleep(1000);
             
             } catch (InterruptedException e) {
                 
