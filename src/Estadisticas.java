c5f45f9c14355fe4c8891a7f0b84057de9b18fda
FranciscoAlonsoE <francisco.alonsoe@correo.ucu.edu.uy>
2025-06-30 00:04:07 -0300

    Clase Estadsiticas para llevar conteo de las estadsiticas de la simulacion

---
+++ src/Estadisticas.java
@@ -0,0 +1,19 @@
+public class Estadisticas {
+    private static int totalPacientes = 0;
+    /** Llamar cuando un paciente empieza a ser atendido */
+    public synchronized static void recordPatient() {
+        totalPacientes++;
+    }
+
+    /** Al final de toda la simulación */
+    public synchronized static void report() {
+        double promPacientes    = totalPacientes > 0 
+                           ? (double) totalPacientes    / 12 
+                           : 0;
+
+        Logger.log("=== ESTADISTICAS DE LA SIMULACIÓN ===");
+        Logger.log("Total pacientes atendidos: " + totalPacientes);
+        Logger.log(String.format("Pacientes atendidos por hora promedio:", promPacientes));
+        Logger.log("===========================");
+    }
+}
